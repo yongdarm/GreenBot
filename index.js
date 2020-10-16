0a1f259022060fb700c16891652a1ba368cd88ea
green1052 <cydyellowgreen@gmail.com>
2020-10-16 17:02:35 +0900

    각종 기능 추가

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
 const Discord = require("discord.js");
-const {MessageEmbed} = require("discord.js");
 const client = new Discord.Client();
 
 const moment = require("moment");
 moment.locale("ko");
 
-const request = require("request");
 const cheerio = require("cheerio");
 const xml2js = require("xml2js");
 const math = require("math-expression-evaluator");
+const axios = require("axios");
 
 const prefix = process.env.prefix;
 
-function CreateMessageEmbed(setFooter = true) {
-    const embed = new MessageEmbed()
-        .setColor("GREEN");
-
-    if (setFooter)
-        embed.setFooter("Made By green1052#2793", "https://cdn.discordapp.com/avatars/368688044934561792/638749733d2d73f23cf12db43e62d33a.webp?size=256");
-
-    return embed;
+function CreateMessageEmbed() {
+    return new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setFooter("Made By green1052#2793", "https://cdn.discordapp.com/avatars/368688044934561792/638749733d2d73f23cf12db43e62d33a.webp?size=256");
 }
 
 function SendErrorMessage(message, command, error) {
     const embed = CreateMessageEmbed()
         .setTitle(":warning: 오류!")
-        .setDescription(`오류가 발생해 명령어 ${command}이(가) 중단됐습니다. 원인: ${error}`);
+        .setDescription(`오류가 발생해 명령어 ${command}이(가) 중단됐습니다. 사유: ${error}`);
 
     message.channel.send(embed);
 
-    console.log(`오류가 발생해 명령어 ${command}이(가) 중단됐습니다. 원인: ${error}`);
+    console.log(`오류가 발생해 명령어 ${command}이(가) 중단됐습니다. 사유: ${error}`);
 }
 
 function SendNeedSomeArgs(message, command, needArgs) {
     const embed = CreateMessageEmbed()
         .setTitle("인수가 부족합니다.")
-        .addField("사용법", `그린아 ${command} ${needArgs}`);
+        .addField("사용법", `${prefix}${command} ${needArgs}`);
 
     message.channel.send(embed);
 }
@@ -50,7 +45,7 @@ function GetDiscordUser(message, allArgs) {
         message.guild.members.fetch().then(members => {
             members.forEach(member => {
                 if (member.user.id.includes(tempAllArgs) || member.user.username.toLowerCase().includes(tempAllArgs) || member.nickname !== null && member.nickname.toLowerCase().includes(tempAllArgs))
-                    return resolve(member);
+                    return resolve(message.guild.member(member));
             });
         }).finally(() => {
             reject("유저를 찾지 못했습니다.");
@@ -59,30 +54,47 @@ function GetDiscordUser(message, allArgs) {
 }
 
 function SendNekoImage(message, command) {
-    request({
-        url: "https://nekos.life/api/v2/img/neko",
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-        }
-    }, function (error, response, body) {
-        if (error)
-            return SendErrorMessage(message, command, error);
-
-        const json = JSON.parse(body);
-
+    axios.get("https://nekos.life/api/v2/img/neko", {headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"}}).then(response => {
         const embed = CreateMessageEmbed()
-            .setImage(json.url);
+            .setImage(response.data.url);
 
         message.channel.send(embed);
+    }).catch(function (e) {
+        SendErrorMessage(message, command, e.message);
     });
+}
+
+function GetQuoteInContent(content) {
+    let contents = [];
+
+    let open = false;
+    let string = "";
+
+    for (const i in content) {
+        if (content[i] === '"')
+            open = !open;
+
+        if (open && content[i] !== '"')
+            string += content[i];
+
+        else if (string !== "") {
+            contents.push(string);
+            string = "";
+        }
+    }
+
+    if (open)
+        return null;
+
+    return contents;
 }
 
 client.login(process.env.token);
 
 client.on("ready", () => {
-    console.log(`${client.user.tag}이(가) 가동 됐습니다.`);
+    console.log(`${client.user.tag}이(가) 가동됐습니다.`);
 
-    client.user.setActivity("그린아 도움말로 도움말 보기", {
+    client.user.setActivity(`${prefix}도움말로 도움말 보기`, {
         type: "PLAYING"
     });
 });
@@ -90,7 +102,7 @@ client.on("ready", () => {
 client.on("message", message => {
     if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-    const args = message.content.slice(prefix.length).split(" ");
+    const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift();
 
     let allArgs = args[0];
@@ -101,18 +113,22 @@ client.on("message", message => {
     if (command === "도움말") {
         const embed = CreateMessageEmbed()
             .setTitle(`${client.user.username} 도움말`)
-            .setDescription(`사용법: ${prefix}(명령어)\n인수 설명: (필수) [선택]`)
+            .setDescription(`예시: ${prefix}(명령어) (필수) [선택]`)
             .addField("도움말", "도움말을 볼 수 있습니다.")
             .addField("핑", "핑을 구할 수 있습니다.")
             .addField("실검", "네이버 실시간 검색어를 볼 수 있습니다.")
-            .addField("유저", "유저 정보를 볼 수 있습니다.")
-            .addField("서버", `${client.user.username}을(를) 사용하는 서버를 볼 수 있습니다.`)
+            .addField("유저 (멘션 | ID | 이름)", "유저 정보를 볼 수 있습니다.")
+            .addField("사용", `${client.user.username}을(를) 사용하는 서버를 볼 수 있습니다.`)
             .addField("초대", `${client.user.username} 초대 링크를 볼 수 있습니다.`)
             .addField("업타임", `${client.user.username}의 작동 시간을 볼 수 있습니다.`)
             .addField("한강", "한강 물 온도를 볼 수 있습니다.")
-            .addField("날씨", "날씨를 볼 수 있습니다.")
-            .addField("계산", "간단한 계산을 할 수 있습니다. [사용법](https://bugwheels94.github.io/math-expression-evaluator/#supported-maths-symbols)")
-            .addField("네코", "귀여운 고양이 소녀를 볼 수 있습니다.");
+            .addField("날씨 (지역)", "날씨를 볼 수 있습니다.")
+            .addField("계산 (식)", "간단한 계산을 할 수 있습니다. [사용법](https://bugwheels94.github.io/math-expression-evaluator/#supported-maths-symbols)")
+            .addField("네코 [반복]", "귀여운 고양이 소녀를 볼 수 있습니다.")
+            .addField(`골라 ("내용")`, "여러 단어 중 한개를 무작위로 골라줍니다.")
+            .addField(`추방 "(멘션 | ID | 이름)" "[사유]"`, "유저를 서버에서 추방할 수 있습니다.")
+            .addField(`밴 "(멘션 | ID | 이름)" "[사유]"`, "유저를 서버에서 밴할 수 있습니다.")
+            .addField("청소 (수)", "메시지를 자울 수 있습니다.");
 
         message.channel.send(embed);
     } else if (command === "핑") {
@@ -124,53 +140,45 @@ client.on("message", message => {
             msg.edit(embed);
         });
     } else if (command === "실검") {
-        request({
-            url: "https://datalab.naver.com/keyword/realtimeList.naver?age=all&where=main",
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-            }
-        }, function (error, response, body) {
-            if (error)
-                return SendErrorMessage(message, command, error);
-
-            const $ = cheerio.load(body);
+        axios.get("https://datalab.naver.com/keyword/realtimeList.naver?age=all&where=main", {headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"}}).then(response => {
+            const $ = cheerio.load(response.data);
 
             const embed = CreateMessageEmbed()
-                .setAuthor("네이버 실시간 검색어 (1위 ~ 10위)", "https://www.naver.com/favicon.ico")
+                .setAuthor("네이버 실시간 검색어 (1위 ~ 20위)", "https://www.naver.com/favicon.ico")
                 .setDescription(`${$(".date_txt").text()} ${$(".time_txt").text()} 기준`);
 
-            $("ul.ranking_list:nth-child(1) span.item_title").each(function (v, k) {
+            $("ul.ranking_list span.item_title").each(function (v, k) {
                 const word = $(k).text();
                 embed.addField(`${v + 1}위`, `[${word}](https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${encodeURI(word)})`, true);
             });
 
             message.channel.send(embed);
+        }).catch(function (e) {
+            SendErrorMessage(message, command, e.message);
         });
     } else if (command === "유저") {
         if (!args[0])
             return SendNeedSomeArgs(message, command, "(멘션 | ID | 이름)");
 
         GetDiscordUser(message, allArgs).then(data => {
-            const member = message.guild.member(data);
-
             const embed = CreateMessageEmbed()
-                .setThumbnail(member.user.avatarURL())
-                .setTitle(`${member.user.tag}님의 유저 정보`)
-                .addField("멘션", member, true)
-                .addField("ID", member.id, true)
-                .addField("별명", member.nickname === null ? "없음" : member.nickname, true)
-                .addField("봇 유무", member.user.bot, true)
-                .addField("역할", member.roles.cache.map(role => role).join("\n"), true)
-                .addField("상태", member.presence.status, true)
-                .addField("서버 접속 일", moment.utc(member.joinedAt).format("YYYY년 MM월 DD일"), true)
-                .addField("계정 생성 일", moment.utc(member.createdAt).format("YYYY년 MM월 DD일"), true)
-                .addField("게임 활동", member.presence.activities.length === 0 ? "없음" : member.presence.activities.map(presence => presence.name).join("\n"), true)
+                .setThumbnail(data.user.avatarURL())
+                .setTitle(`${data.user.tag}님의 유저 정보`)
+                .addField("멘션", data, true)
+                .addField("ID", data.id, true)
+                .addField("별명", data.nickname === null ? "없음" : data.nickname, true)
+                .addField("봇 유무", data.user.bot, true)
+                .addField("역할", data.roles.cache.map(role => role).join("\n"), true)
+                .addField("상태", data.presence.status, true)
+                .addField("서버 접속 일", moment.utc(data.joinedAt).format("LL"), true)
+                .addField("계정 생성 일", moment.utc(data.createdAt).format("LL"), true)
+                .addField("게임 활동", data.presence.activities.length === 0 ? "없음" : data.presence.activities.map(presence => presence.name).join("\n"), true)
 
             message.channel.send(embed);
         }, function (error) {
             SendErrorMessage(message, command, error);
         });
-    } else if (command === "서버") {
+    } else if (command === "사용") {
         const embed = CreateMessageEmbed()
             .setTitle(`${client.user.username}을(를) 사용하는 서버 목록`)
             .setDescription(`${client.guilds.cache.size}개의 서버가 봇을 사용하고 있습니다.`);
@@ -199,37 +207,21 @@ client.on("message", message => {
 
         message.channel.send(embed);
     } else if (command === "한강") {
-        request({
-            url: "http://hangang.dkserver.wo.tc/",
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-            }
-        }, function (error, response, body) {
-            if (error)
-                return SendErrorMessage(error);
-
-            const json = JSON.parse(body);
-
+        axios.get("http://hangang.dkserver.wo.tc/", {headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"}}).then(response => {
             const embed = CreateMessageEmbed()
-                .setTitle(`:man_playing_water_polo: ${moment(json.time).format("YYYY년 MM월 DD일 a hh:mm:ss")} 기준 한강 온도는`)
-                .setDescription(`${json.temp}°C 입니다.`)
+                .setTitle(`:man_playing_water_polo: ${moment(response.data.time).format("LLLL")} 기준 한강 물 온도`)
+                .setDescription(`${response.data.temp}°C`)
 
             message.channel.send(embed);
+        }).catch(function (e) {
+            SendErrorMessage(message, command, e.message);
         });
     } else if (command === "날씨") {
         if (!args[0])
-            return SendNeedSomeArgs("(위치)");
+            return SendNeedSomeArgs(message, command, "(위치)");
 
-        request({
-            url: `http://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=C&culture=ko-KR&weasearchstr=${encodeURI(allArgs)}`,
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-            }
-        }, function (error, response, body) {
-            if (error)
-                return SendErrorMessage(message, command, error);
-
-            xml2js.parseString(body, function (err, result) {
+        axios.get(`http://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=C&culture=ko-KR&weasearchstr=${encodeURI(allArgs)}`, {headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"}}).then(response => {
+            xml2js.parseString(response.data, function (err, result) {
                 if (err)
                     return SendErrorMessage(message, command, err);
 
@@ -238,7 +230,7 @@ client.on("message", message => {
 
                 const embed = CreateMessageEmbed()
                     .setThumbnail(`${weather.$.imagerelativeurl}law/${current.skycode}.gif`)
-                    .setTitle(`${moment(`${current.date} ${current.observationtime}`).format("YYYY년 MM월 DD일 a hh:mm:ss")} 기준 ${current.observationpoint} 날씨`)
+                    .setTitle(`${moment(`${current.date} ${current.observationtime}`).format("LLLL")} 기준 ${current.observationpoint} 날씨`)
                     .addField("현재 날씨", current.skytext, true)
                     .addField("온도", `${current.temperature}°C`, true)
                     .addField("체감 온도", `${current.feelslike}°C`, true)
@@ -248,6 +240,8 @@ client.on("message", message => {
 
                 message.channel.send(embed);
             });
+        }).catch(function (e) {
+            SendErrorMessage(message, command, e.message);
         });
     } else if (command === "계산") {
         if (!args[0])
@@ -255,7 +249,7 @@ client.on("message", message => {
 
         try {
             const embed = CreateMessageEmbed()
-                .setTitle("계산 결과")
+                .setTitle("결과")
                 .setDescription(math.eval(allArgs));
 
             message.channel.send(embed);
@@ -264,21 +258,103 @@ client.on("message", message => {
         }
     } else if (command === "네코") {
         if (args[0]) {
-            let num = Number(args[0]);
-
-            if (isNaN(num))
+            if (isNaN(args[0]))
                 return SendErrorMessage(message, command, "숫자가 아닙니다.");
 
-            if (num > 10)
-                return SendErrorMessage(message, command, "최대 10번만 반복할 수 있습니다.");
+            if (args[0] > 5)
+                return SendErrorMessage(message, command, "최대 5번만 반복할 수 있습니다.");
 
-            for (let i = 0; i < num; i++) {
+            for (let i = 0; i < args[0]; i++)
                 SendNekoImage(message, command);
-            }
 
             return;
         }
 
         SendNekoImage(message, command);
+    } else if (command === "골라") {
+        if (!args[0] || !args[1])
+            return SendNeedSomeArgs(message, command, `"(단어)"`);
+
+        const contents = GetQuoteInContent(message.content);
+
+        if (contents === null)
+            return SendErrorMessage(message, command, "배열이 잘못됐습니다.");
+
+        const embed = CreateMessageEmbed()
+            .setTitle(contents[Math.floor(Math.random() * contents.length)]);
+
+        message.channel.send(embed);
+    } else if (command === "추방") {
+        if (!message.guild.member(message.author).hasPermission("KICK_MEMBERS"))
+            return SendErrorMessage(message, command, "권한이 부족합니다.");
+
+        if (!args[0])
+            return SendNeedSomeArgs(message, command, `"(멘션 | ID | 이름)" "[사유]"`);
+
+        const contents = GetQuoteInContent(message.content);
+
+        if (contents === null)
+            return SendErrorMessage(message, command, "배열이 잘못됐습니다.");
+
+        GetDiscordUser(message, contents[0]).then(data => {
+            data.kick(contents[1]).then(member => {
+                const embed = CreateMessageEmbed()
+                    .setTitle(`${member.user.tag}을(를) 성공적으로 추방 했습니다.`)
+                    .setDescription(`사유: ${contents[1] === null ? contents[1] : "없음"}`);
+
+                message.channel.send(embed);
+            }).catch(error => {
+                SendErrorMessage(message, command, error);
+            });
+
+        }, function (error) {
+            SendErrorMessage(message, command, error);
+        });
+    } else if (command === "밴") {
+        if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS"))
+            return SendErrorMessage(message, command, "권한이 부족합니다.");
+
+        if (!args[0])
+            return SendNeedSomeArgs(message, command, `"(멘션 | ID | 이름)" "[사유]"`);
+
+        const contents = GetQuoteInContent(message.content);
+
+        if (contents === null)
+            return SendErrorMessage(message, command, "배열이 잘못됐습니다.");
+
+        GetDiscordUser(message, contents[0]).then(data => {
+            data.ban(contents[1]).then(member => {
+                const embed = CreateMessageEmbed()
+                    .setTitle(`${member.user.tag}을(를) 성공적으로 밴 했습니다.`)
+                    .setDescription(`사유: ${contents[1] === null ? contents[1] : "없음"}`);
+
+                message.channel.send(embed);
+            }).catch(error => {
+                SendErrorMessage(message, command, error);
+            });
+
+        }, function (error) {
+            SendErrorMessage(message, command, error);
+        });
+    } else if (command === "청소") {
+        if (!args[0])
+            return SendNeedSomeArgs(message, command, "(수)");
+
+        if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES"))
+            return SendErrorMessage(message, command, "권한이 부족합니다.");
+
+        if (isNaN(args[0]))
+            return SendErrorMessage(message, command, "숫자가 아닙니다.");
+
+        if (args[0] > 100)
+            return SendErrorMessage(message, command, "100 이하의 값을 입력 해주세요");
+
+        message.delete();
+
+        message.channel.bulkDelete(args[0]).then(messages => {
+            message.channel.send(CreateMessageEmbed().setTitle("삭제 완료").setDescription(`${messages.size}개의 메시지를 지웠습니다.`))
+        }).catch(error => {
+            SendErrorMessage(message, command, error);
+        });
     }
 });
